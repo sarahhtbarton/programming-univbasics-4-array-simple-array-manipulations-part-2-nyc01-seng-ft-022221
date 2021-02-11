50943bfda554a61476537c09eed52ad36698b630
Sarah Barton <sarahhtbarton@gmail.com>
2021-02-11 18:32:02 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,10 @@
+def using_concat(array1, array2)
+  new_array = array1.concat(array2)
+end
+puts using_concat([1, 2, 3], [4, 5, 6])
+
+def using_insert(array, element)
+  array.insert(4, element)
+  return array
+end
+puts using_concat([1, 2, 3, 4, 5, 6], 9)
